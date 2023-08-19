b3a6ab1bd6b0ca52d9870dfa98d70d5202e4f680
mbr1993 <boburmannonov93@gmail.com>
2023-08-19 09:56:48 +0500

    added stars

--- app/src/main/java/uz/mbr/quiz/ui/TestFragment.kt
+++ app/src/main/java/uz/mbr/quiz/ui/TestFragment.kt
@@ -21,39 +21,48 @@ class TestFragment : Fragment() {
         val root: View = binding.root
 
         with(binding) {
-            btnScore.setOnClickListener {
+            btnClose.setOnClickListener {
                 findNavController().navigate(R.id.action_testFragment_to_navigation_home)
             }
 
             question1.setOnClickListener {
-                question1.cardElevation = 100F
+                question1.setCardBackgroundColor(resources.getColor(R.color.md_theme_primaryContainer))
+                question1.strokeWidth = 2
+
+                question2.setCardBackgroundColor(resources.getColor(R.color.md_theme_background))
+                question3.setCardBackgroundColor(resources.getColor(R.color.md_theme_background))
+//                checkQuestion.setCardBackgroundColor(resources.getColor(R.color.md_theme_tertiaryContainer))
+                btnCheck.visibility = View.VISIBLE
+
+                question1.cardElevation = 50F
                 question2.cardElevation = 1F
                 question3.cardElevation = 1F
-                question1.strokeWidth = 2
-                question2.strokeWidth = 0
-                question3.strokeWidth = 0
-
-                checkQuestion.setCardBackgroundColor(resources.getColor(R.color.md_theme_tertiaryContainer))
             }
             question2.setOnClickListener {
-                question2.cardElevation = 100F
-                question1.cardElevation = 1F
-                question3.cardElevation = 1F
+                question2.setCardBackgroundColor(resources.getColor(R.color.md_theme_primaryContainer))
                 question2.strokeWidth = 2
-                question1.strokeWidth = 0
-                question3.strokeWidth = 0
 
-                checkQuestion.setCardBackgroundColor(resources.getColor(R.color.md_theme_tertiaryContainer))
+                question1.setCardBackgroundColor(resources.getColor(R.color.md_theme_background))
+                question3.setCardBackgroundColor(resources.getColor(R.color.md_theme_background))
+//                checkQuestion.setCardBackgroundColor(resources.getColor(R.color.md_theme_tertiaryContainer))
+                btnCheck.visibility = View.VISIBLE
+
+                question1.cardElevation = 1F
+                question2.cardElevation = 50F
+                question3.cardElevation = 1F
             }
             question3.setOnClickListener {
-                question3.cardElevation = 100F
-                question2.cardElevation = 1F
-                question1.cardElevation = 1F
+                question3.setCardBackgroundColor(resources.getColor(R.color.md_theme_primaryContainer))
                 question3.strokeWidth = 2
-                question2.strokeWidth = 0
-                question1.strokeWidth = 0
 
-                checkQuestion.setCardBackgroundColor(resources.getColor(R.color.md_theme_tertiaryContainer))
+                question1.setCardBackgroundColor(resources.getColor(R.color.md_theme_background))
+                question2.setCardBackgroundColor(resources.getColor(R.color.md_theme_background))
+//                checkQuestion.setCardBackgroundColor(resources.getColor(R.color.md_theme_tertiaryContainer))
+                btnCheck.visibility = View.VISIBLE
+
+                question1.cardElevation = 1F
+                question2.cardElevation = 1F
+                question3.cardElevation = 50F
             }
         }
 
